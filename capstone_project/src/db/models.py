c01c0de4efe5868a9e3561f4ae30614282a340e0
Daniel Perez <daniel_p0818@hotmail.com>
2025-12-30 14:05:53 -0500

    feat: Define UserModel with SQLAlchemy ORM for user data management

---
+++ capstone_project/src/db/models.py
@@ -0,0 +1,12 @@
+"""module to define database models with SQLAlchemy ORM"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    age = Column(Integer, nullable=False)
