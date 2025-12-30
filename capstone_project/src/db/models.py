77d0d6f1a8e464231782224b34c533062700dd12
Daniel Perez <daniel_p0818@hotmail.com>
2025-12-30 14:08:15 -0500

    style: Format UserModel table name string and add missing newlines

--- capstone_project/src/db/models.py
+++ capstone_project/src/db/models.py
@@ -1,10 +1,13 @@
 """module to define database models with SQLAlchemy ORM"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
+
 class UserModel(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
